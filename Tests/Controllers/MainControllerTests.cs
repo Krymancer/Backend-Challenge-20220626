ffaef468f8b3f0814d837ed52260407cc56c9585
Júnior Nascimento <junior.nascm@gmail.com>
2023-01-05 20:40:45 -0300

    feature: Unit Tests

---
+++ Tests/Controllers/MainControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using Backend_Challenge_20220626.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tests.Controllers
+{
+    public class MainControllerTests
+    {
+        private readonly MainController _controller;
+        public MainControllerTests()
+        {
+            _controller = new MainController();
+        }
+
+        [Fact]
+        public void ShouldCreate()
+        {
+            _controller.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task Should_Return_Message()
+        {
+            // Arrange
+
+            // Setup
+
+            // Act
+
+            var result = _controller.Get() as OkObjectResult;
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Value.Should().Be("Fullstack Challenge 20201026");
+        }
+    }
+}
